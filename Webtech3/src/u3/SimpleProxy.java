158a2f45bef2d97cc08fb8b887366e52f18277c7
Zcience <david.liebemann@gmail.com>
2015-11-21 06:27:12 +0100

    Design is done for now

--- Webtech3/src/u3/SimpleProxy.java
+++ Webtech3/src/u3/SimpleProxy.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.regex.Pattern;
 
 public class SimpleProxy implements Runnable
