9c0cadd5c3504a5ffdac6966414ef54156296c2b
kevin2751992 <ke-messmer@gmx.de>
2015-12-01 09:20:01 +0100

    added needed files

---
+++ Webtech3/src/u7/filterImg.js
@@ -0,0 +1,22 @@
+
+function filterImg()
+	{
+		
+		var images =document.getElementsByTagName("img");
+		
+		var alt;
+				
+		for(var i = 0; i < images.length; i)
+					
+		{		
+			alt = document.createElement("p");
+			
+			alt.innerHTML = images[i].alt;
+			
+			images[i].parentNode.replaceChild(alt, images[i]);
+			
+			
+			  	 
+		}
+	}
+			
